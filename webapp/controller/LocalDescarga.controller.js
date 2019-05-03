bb45b14d7a80e874ed79888180d9e8a5780f958d
matusca96 <gomesloud@gmail.com>
2019-05-03 10:51:39 -0300

    Atualização.

--- webapp/controller/LocalDescarga.controller.js
+++ webapp/controller/LocalDescarga.controller.js
@@ -18,14 +18,16 @@ sap.ui.define([
 			this.getOwnerComponent().setModel(oJSONModel, "model");
 			this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
 			
-			this.getModel().attachMetadataLoaded(function(){
-				var oFilter = new Filter("Empresa", FilterOperator.EQ, Session.get("EMPRESA_ID"));
-				var oView = this.getView();
-				var oTable = oView.byId("tableLocal");
-				var oColumn = oView.byId("columnDescricao");
-				
-				oTable.sort(oColumn);
-				oView.byId("tableLocal").getBinding("rows").filter(oFilter, "Application");
+			var oFilter = new Filter("Empresa", FilterOperator.EQ, Session.get("EMPRESA_ID"));
+			var oView = this.getView();
+			var oTable = oView.byId("tableLocal");
+			
+			oTable.bindRows({
+				path: '/LocalDescargas',
+				sorter: {
+					path: 'Descricao'
+				},
+				filters: oFilter
 			});
 		},
 		
@@ -33,7 +35,7 @@ sap.ui.define([
 			var sQuery = oEvent.getParameter("query");
 			var oFilter1 = new Filter("Empresa", FilterOperator.EQ, Session.get("EMPRESA_ID"));
 			var oFilter2 = new Filter("Descricao", FilterOperator.Contains, sQuery);
-			
+
 			var aFilters = [
 				oFilter1,
 				oFilter2
